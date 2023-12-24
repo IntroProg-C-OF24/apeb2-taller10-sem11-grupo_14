9ad2363ce1dbb07d2a05b3ccde3c517e1cfc4b56
pablo <pablo.velezleon1@icloud.com>
2023-12-24 18:21:21 -0500

    TALLER #11/ PABLO

---
+++ SOLUCION_MATRICES/Soluciones_taller11/src/Ejercicio10.java
@@ -0,0 +1,28 @@
+public class Ejercicio10 {
+    public static void main(String[] args) {
+        int altura = 7;
+        for (int i = 0; i < altura; i++) {
+            for (int j = 0; j < altura - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+        for (int i = altura - 2; i >= 0; i--) {
+            for (int j = 0; j < altura - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
+
+            
+        
+    
+
