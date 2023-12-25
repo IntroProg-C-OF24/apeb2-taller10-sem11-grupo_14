4d6c727b2e6a516d5ae4f336d1993fd0d3014c7a
pablo <pablo.velezleon1@icloud.com>
2023-12-24 20:26:50 -0500

    TALLER #11/ PABLO

---
+++ SOLUCION_MATRICES/Soluciones_taller11/src/ejercicio08.java
@@ -0,0 +1,43 @@
+import java.util.Random;
+public class ejercicio08 {
+    public static void main(String[] args) {
+        int numero = 5;
+        int[][] matriz = new int[numero][numero];
+        Random random = new Random();
+        for (int i = 0; i < numero; i++) {
+            for (int j = 0; j < numero; j++) {
+                matriz[i][j] = random.nextInt(19) - 9;
+            }
+        }
+
+        System.out.println("Matriz generada:");
+        for (int i = 0; i < numero; i++) {
+            for (int j = 0; j < numero; j++) {
+                System.out.printf("%3d ", matriz[i][j]);
+            }
+            System.out.println("");
+        }
+
+        System.out.println("\nElementos de la diagonal principal:");
+        for (int i = 0; i < numero; i++) {
+            System.out.printf("%3d ", matriz[i][i]);
+        }
+
+        System.out.println("\nElementos sobre la diagonal principal:");
+        for (int i = 0; i < numero; i++) {
+            for (int j = i + 1; j < numero; j++) {
+                System.out.printf("%3d ", matriz[i][j]);
+            }
+        }
+
+        System.out.println("\nElementos bajo la diagonal principal:");
+        for (int i = 1; i < numero; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.printf("%3d ", matriz[i][j]);
+            }
+        }
+    }
+}
+/*
+
+*/
